985bc34755139b4205f6a3220aa8e26b1eddcd0b
johnmogi <anguru@gmail.com>
2019-09-22 13:55:19 +0300

    no set cleartimeout yet

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
-// 0. library: defining the order of actions:
-
-// 1. Declaring the Elements from the DOM:
-// 2. passing a value from within a scope to further reuse:
-// 3. cleaning the canvas
-// 4. validating the form
-// 5. calculating the circle radius
-// 6. genereting the actual canvas circle after calculation
-
-// 1. Declaring the Elements from the DOM:
 
 
-// let isReady = document.readyState
+document.addEventListener("DOMContentLoaded",start) 
 
-// if (isReady == true){ 
 
-function startjs(){ 
+function start(){
+
+  library();
+  cleanCV();
+  validate();
+  calculate();
+}
+
+function library(){ 
+  var elem = document.getElementById("geneteBut");
 const ballCreate = document.getElementById("ballCreate")
 const ballC = document.getElementById("ballC")
 const ballCalcul = document.getElementById("ballCalcul")
@@ -24,45 +22,33 @@ const stopGen = document.getElementById("stopGen")
 const ballOutput = document.getElementById("ballOutput")
 const stagetext = document.getElementById("stagetext")
 
-
-// 2. passing a value from within a scope to further reuse:
-
-
 let volume = 1;
-// let interval = ""
-// validate
-// setting action buttons
-submitBut.addEventListener("click", validate)
+
+elem.addEventListener("click", change)
 cleanCanvas.addEventListener("click", cleanCV)
-// stopGen.addEventListener("click", clearInterval(randomCircles))
-
-// 3. cleaning the canvas
-
-
+submitBut.addEventListener("click", validate)
+}
 function cleanCV(){
   const context = myCanvas.getContext('2d');
   context.clearRect(0, 0, myCanvas.width, myCanvas.height);
 }
 
-// 4. validating the form
 
 
-function validate(e){ 
+function validate(){ 
 
-    e.preventDefault
+  // preventDefault
 if (ballCalcul.value === ""){
   stagetext.innerText= ("no field value")
   stagetext.style.color = ("red")
   ballCalcul.style.border = ("red")
-    // alert("no field value")
-// ballCalcul.style.border="#f00"
+
 }
 if(isNaN(ballCalcul.value)){
   stagetext.innerText= ("Please input a number")
   stagetext.style.color = ("red")
   ballCalcul.style.border = ("red")
 
-    // alert("Please input a number")
 }
 else{
 calculate();
@@ -73,13 +59,6 @@ return
 generate();
 }
 }
-
-//volume = number | PIE R (3)/ 3
-// 23*4*3.14/3 = 8*4*3.14/3=33.49333
-
-
-// 5. calculating the circle radius
-
 function calculate(){
         const base = (ballCalcul.value*4) 
         const pieCalc = (base*3.14)/3 
@@ -88,16 +67,16 @@ function calculate(){
     
 }
 
-// 6. genereting the actual canvas circle after calculation
-
 function generate(){
 
   var canvas = document.getElementById('myCanvas');
   var context = canvas.getContext('2d');
   var centerX = canvas.width / 2;
   var centerY = canvas.height / 2;
+
+  let volume =  Math.floor(Math.random() * 397) + 1  
   var radius = volume;
-// console.log(radius)
+
   context.beginPath();
   context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
   context.fillStyle = '#B7B4B5';
@@ -105,6 +84,22 @@ function generate(){
   context.lineWidth = 5;
   context.strokeStyle = '#757374';
   context.stroke();
+ 
 
 }
+// http://jsfiddle.net/67ct5sre/20/
+
+
+
+function change(){
+  
+
+
+  var myVar = setInterval(generate, 100);
+
+function myStopFunction() {
+  clearInterval(myVar);
+}
+
+
 }
